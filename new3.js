1083a22d5db2335314eb6e3e5ebcf86bc060767b
ariba-dot <aribashafeeque@gmail.com>
2025-01-24 12:56:13 +0530

    first commit

--- new3.js
+++ new3.js
@@ -1,7 +1,34 @@
-const students={name:"roy",college:"npgc"}
-function studentsdetails(){
-    console.log(students);
+// const students={name:"roy",college:"npgc"}
+// function studentsdetails(){
+//     console.log(students);
+// }
+// studentsdetails();
+// students.class="bvoc";
+// studentsdetails();
+const number=[1,2,3,4,5,6,7,8,9,10]
+
+function funOne() {
+    console.log(number);
+    let newarray=[];
+    for (let i = 0; i < number.length; i++){
+        
+            newarray.push(number[i]*2);
+        
+        
+    }
+    return newarray;
 }
-studentsdetails();
-students.class="bvoc";
-studentsdetails();
+function fun2(action) {
+    let result=action();
+    console.log("result",result);
+    
+}
+//higher order function
+fun2(funOne);
+
+
+function fun3(){
+    const n = [1, 2, 3, 4, 5];
+    const even = n.filter((num) => num % 2 === 0);
+    console.log(even);
+}
