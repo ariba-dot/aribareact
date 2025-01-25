59582e1d0edf2f6760e03b07f2f4bc6af1abec4c
ariba-dot <aribashafeeque@gmail.com>
2025-01-25 12:47:28 +0530

    first commit

--- new3.js
+++ new3.js
@@ -5,30 +5,27 @@
 // studentsdetails();
 // students.class="bvoc";
 // studentsdetails();
-const number=[1,2,3,4,5,6,7,8,9,10]
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function funOne() {
-    console.log(number);
-    let newarray=[];
-    for (let i = 0; i < number.length; i++){
-        
-            newarray.push(number[i]*2);
-        
-        
-    }
-    return newarray;
+  console.log(number);
+  let newarray = [];
+  for (let i = 0; i < number.length; i++) {
+    newarray.push(number[i] * 2);
+  }
+  return newarray;
 }
 function fun2(action) {
-    let result=action();
-    console.log("result",result);
-    
+  let result = action();
+  console.log("result", result);
+
+  
 }
 //higher order function
 fun2(funOne);
 
-
-function fun3(){
-    const n = [1, 2, 3, 4, 5];
-    const even = n.filter((num) => num % 2 === 0);
-    console.log(even);
+function fun3() {
+  const n = [1, 2, 3, 4, 5];
+  const even = n.filter((num) => num % 2 === 0);
+  console.log(even);
 }
